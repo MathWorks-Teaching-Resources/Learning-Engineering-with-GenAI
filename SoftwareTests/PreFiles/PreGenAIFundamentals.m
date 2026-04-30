b9d92ce5070bb6397643a6d1cbc6c4e8bae6ae83
callie <callie@mathworks.com>
2026-04-29 19:13:27 -0500

    Release ready version of Learning Engineering with GenAI. Initial Release.

---
+++ SoftwareTests/PreFiles/PreGenAIFundamentals.m
@@ -0,0 +1,5 @@
+%  Pre-run script for GenAIFundamentals.mlx
+% ---- Known Issues     -----
+KnownIssuesID = "llms:mustBeMessagesOrTxt";
+% ---- Pre-run commands -----
+Prompt = "Create a plot for the variables X and Y using MATLAB";
---
+++ SoftwareTests/PreFiles/PreGenAIFundamentalsSoln.m
@@ -0,0 +1,5 @@
+%  Pre-run script for GenAIFundamentalsSoln.mlx
+% ---- Known Issues     -----
+KnownIssuesID = "llms:mustBeMessagesOrTxt";
+% ---- Pre-run commands -----
+Prompt = "Create a plot for the variables X and Y using MATLAB";
