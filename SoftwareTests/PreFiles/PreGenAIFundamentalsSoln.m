7fde8afb90e8f90a5beb9930d2469441ee30ae07
callie <callie@mathworks.com>
2026-05-01 16:54:20 -0500

    Complete testing is now passing for 2026a. This version is being pushed with the newsletter.

--- SoftwareTests/PreFiles/PreGenAIFundamentalsSoln.m
+++ SoftwareTests/PreFiles/PreGenAIFundamentalsSoln.m
@@ -3,18 +3,4 @@
 KnownIssuesID = ["llms:mustBeMessagesOrTxt";"MATLAB:UndefinedFunction"];
 % ---- Pre-run commands -----
 
-% Chat = "N/A";
-% LLM_Response = "N/A";
-% ollamaChat = @(x) disp("... Running Ollama "+x);
-% generate = @(x) disp("... Running Ollama "+x);
-% openAIFunction = @(x) disp("... Running Ollama "+x);
-% addParameter = @(x) disp("... Running Ollama "+x);
-
-% NData = 50;
-% X = linspace(-5,5,NData)';
-% Y = X.^2;
-% 
-% Response.tool_calls.function.arguments.X = X;
-% Response.tool_calls.function.arguments.Y = Y;
-
 Prompt = "Create a plot for the variables X and Y using MATLAB";
